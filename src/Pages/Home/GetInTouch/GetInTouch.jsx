dcbf27a1472ad4f7ec947fa7e78482a2b899d4dc
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2023-05-03 21:19:55 +0600

    react lotti added

--- src/Pages/Home/GetInTouch/GetInTouch.jsx
+++ src/Pages/Home/GetInTouch/GetInTouch.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import Lottie from "lottie-react";
+import bye from '../../../assets/bye.json'
 
 const GetInTouch = () => {
   return (
     <div className=" py-24 mx-auto bg-gray-100">
-      <h2 className="font-medium text-6xl mb-8 text-center ">Get IN Touch</h2>
-      <form className="my-container w-3/4 md:w-1/2 ">
-        <div className="form-group mb-6">
-          <input
-            type="text"
-            className="form-control block
+      <h2 className="font-medium text-6xl mb-8 my-container">Get IN Touch</h2>
+      <div className="md:flex justify-between items-center my-container ">
+        <form className="md:w-1/2">
+          <div className="form-group mb-6">
+            <input
+              type="text"
+              className="form-control block
                 w-full
                 px-3
                 py-1.5
@@ -22,14 +25,14 @@ const GetInTouch = () => {
                 ease-in-out
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            id="exampleInput7"
-            placeholder="Name"
-          />
-        </div>
-        <div className="form-group mb-6">
-          <input
-            type="email"
-            className="form-control block
+              id="exampleInput7"
+              placeholder="Name"
+            />
+          </div>
+          <div className="form-group mb-6">
+            <input
+              type="email"
+              className="form-control block
                 w-full
                 px-3
                 py-1.5
@@ -43,13 +46,13 @@ const GetInTouch = () => {
                 ease-in-out
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            id="exampleInput8"
-            placeholder="Email address"
-          />
-        </div>
-        <div className="form-group mb-6">
-          <textarea
-            className="
+              id="exampleInput8"
+              placeholder="Email address"
+            />
+          </div>
+          <div className="form-group mb-6">
+            <textarea
+              className="
                 form-control
                 block
                 w-full
@@ -66,18 +69,18 @@ const GetInTouch = () => {
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
               "
-            id="exampleFormControlTextarea13"
-            rows="3"
-            placeholder="Message"
-          ></textarea>
-        </div>
-        <div className="form-group form-check text-center mb-6"></div>
-        <button
-          type="submit"
-          className="
+              id="exampleFormControlTextarea13"
+              rows="3"
+              placeholder="Message"
+            ></textarea>
+          </div>
+          <div className="form-group form-check text-center mb-6"></div>
+          <button
+            type="submit"
+            className="
               w-full
               px-6
-              py-2.5
+              py-4
               bg-blue-600
               text-white
               font-medium
@@ -92,10 +95,14 @@ const GetInTouch = () => {
               transition
               duration-150
               ease-in-out"
-        >
-          Send
-        </button>
-      </form>
+          >
+            Send
+          </button>
+        </form>
+        <div>
+            <Lottie animationData={bye} loop={true}></Lottie>
+        </div>
+      </div>
     </div>
   );
 };
