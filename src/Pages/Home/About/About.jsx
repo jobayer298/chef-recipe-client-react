11946860e9bdbb09b89bea5db9787c3b7f40b060
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2023-05-03 20:43:11 +0600

    readme file added

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import img from  '../../../assets/bon.jfif'
 const About = () => {
     return (
-      <div className="bg-black">
+      <div className="bg-[#272626] py-10">
         <div className="grid md:grid-cols-2 gap-4 my-container py-9  text-white">
-          <div className='md:mt-16'>
+          <div className="md:mt-16">
             <h2 className="font-medium text-6xl text-red-600">Bon appetit!</h2>
-            <p className='my-7'>
+            <p className="my-7">
               We’re all about plants and have been committed to sustainable
               organic farmers since opening our restaurant in 2010. Our entire
               menu is organic, gluten and wheat free with no refined sugars.
@@ -15,7 +15,7 @@ const About = () => {
             </p>
           </div>
           <div>
-            <img className='rounded-lg shadow-2xl' src={img} alt="" />
+            <img className="rounded-lg shadow-2xl" src={img} alt="" />
           </div>
         </div>
       </div>
