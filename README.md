65ea3ec714a6351e23ba4c210eca0562b9cec9e8
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2023-05-04 09:58:42 +0600

    readme file updated

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 "# chef-recipe-jobayer298" 
 # Chef Recipe
 <!-- ## live Link: [https://stellular-douhua-1c822c.netlify.app/](https://stellular-douhua-1c822c.netlify.app/) -->
-### 1. This is a chef recipe site
+## 1. This is a chef recipe site
+
+### 2. First I made a header and footer section for all pages.
+### 3. There is also authentication system. we can login by Email password, google , github.
+### 4. I made a blog section and 6 cards for chefs .
+### 5. If we want to see the chef details then we have to login first because it is a private route.
+### 6. I also added a swiper slider in banner section.
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -34,9 +34,9 @@ const Banner = () => {
               no matter how you cook, <br /> Yumma has many the <br /> recipes
             </h2>
             <p className="font-medium text-[18px] text-gray-200 my-9">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-              tellus, luctus nec <br /> ullamcorper mattis, pulvinar dapibus
-              leo.
+              We’re all about plants and have been committed to sustainable
+              organic farmers since <br /> Our entire menu is organic, gluten
+              and wheat free with no refined sugars.
             </p>
             <button className="btn text-white bg-red-500 border-0">
               Explore More
@@ -51,9 +51,9 @@ const Banner = () => {
               no matter how you cook, <br /> Yumma has many the <br /> recipes
             </h2>
             <p className="font-medium text-[18px] text-gray-200 my-9">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-              tellus, luctus nec <br /> ullamcorper mattis, pulvinar dapibus
-              leo.
+              We’re all about plants and have been committed to sustainable
+              organic farmers since <br /> Our entire menu is organic, gluten
+              and wheat free with no refined sugars.
             </p>
             <button className="btn text-white bg-red-500 border-0">
               Explore More
